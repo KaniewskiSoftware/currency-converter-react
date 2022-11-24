638d0f483366a636739f827999c958c531ecb252
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2022-11-24 22:31:45 +0100

    clock functionality added

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,9 +1,33 @@
-import "./style.css"
+import { useState, useEffect } from "react";
+import "./style.css";
 
-const Clock = () => (
+const Clock = () => {
+  const [theDate, setTheDate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTheDate(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
     <header className="header">
-        <p className="header__clock"></p>
+      <p className="header__clock">
+        Dzisiaj jest {theDate.toLocaleDateString("pl", { weekday: "long" })}
+        {", "}
+        {theDate.toLocaleDateString("pl", { day: "numeric", month: "long" })}
+        {", "}
+        {theDate.toLocaleDateString("pl", {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        })}
+      </p>
     </header>
-)
+  );
+};
 
 export default Clock;
