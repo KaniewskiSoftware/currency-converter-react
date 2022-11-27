fc3aa94e9037435d7962eb7669f3194450f000bc
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2022-11-27 21:27:33 +0100

    background color changed to lighter grey

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -4,7 +4,7 @@ export const Main = styled.main`
   flex-basis: 900px;
   margin: 20px;
   padding: 20px;
-  background-color: rgb(114, 113, 113);
+  background-color: rgb(143, 142, 142);
   letter-spacing: 1px;
   border: 2px solid #212529;
   border-radius: 10px;
--- src/Result/styled.js
+++ src/Result/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Section = styled.section`
   margin-top: 10px;
   width: 100%;
-  background-color: rgb(114, 113, 113);
+  background-color: rgb(143, 142, 142);
   text-align: center;
   border: 2px solid #212529;
   border-radius: 10px;
