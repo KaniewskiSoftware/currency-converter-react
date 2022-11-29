c7c87a8b9aa1a919e21e6b67ba7cca41725462d1
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2022-11-29 17:05:36 +0100

    Results moved to Form

--- src/App/App.js
+++ src/App/App.js
@@ -1,32 +1,14 @@
-import { useState } from "react";
 import Container from "./Container";
 import Form from "./Form";
-import Result from "./Result";
 import Clock from "./Clock";
 
 function App() {
-  const [result, setResult] = useState({
-    startCurrency: "PLN",
-    endCurrency: "PLN",
-    value: "",
-    rate: 1,
-  });
-
-  const getResult = (startCurrency, endCurrency, value, rate) => {
-    setResult((result) => ({
-      ...result,
-      startCurrency: startCurrency,
-      endCurrency: endCurrency,
-      value: value,
-      rate: rate,
-    }));
-  };
+  
 
   return (
     <Container>
       <Clock />
-      <Form getResult={getResult}/>
-      <Result result={result} />
+      <Form />
     </Container>
   );
 }
--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,29 +1,44 @@
 import { useState, useRef, useEffect } from "react";
+import Result  from "./Result";
 import { Calculator, Fieldset, Legend, Span, Input } from "./styled";
 import { currencies } from "../../currencies/currencies";
 // import { useAPI } from "./useAPI";
-import {rates,status,date} from "./API"
+import { rates, status, date } from "./API";
 
-const Form = ({ getResult}) => {
+const Form = () => {
   // const {rates, status, date} = useAPI();
   const [amount, setAmount] = useState("");
   const [startCurrency, setStartCurrency] = useState("PLN");
   const [endCurrency, setEndCurrency] = useState("USD");
   const ref = useRef();
+  const [result, setResult] = useState({
+    startCurrency: "PLN",
+    endCurrency: "PLN",
+    value: "",
+    rate: 1,
+  });
+
+  const getResult = (startCurrency, endCurrency, value, rate) => {
+    setResult((result) => ({
+      ...result,
+      startCurrency: startCurrency,
+      endCurrency: endCurrency,
+      value: value,
+      rate: rate,
+    }));
+  };
 
   useEffect(() => {
     ref.current.focus();
   }, []);
-  // console.log(rates[`${startCurrency}`])
   const getRate = (rates, startCurrency, endCurrency) => {
     if (startCurrency === endCurrency) {
       return 1;
-    }
-    else if (startCurrency === "PLN") {
-      return rates[`${endCurrency}`]
+    } else if (startCurrency === "PLN") {
+      return rates[`${endCurrency}`];
     }
 
-    return ((1/rates[`${startCurrency}`])*rates[`${endCurrency}`])
+    return (1 / rates[`${startCurrency}`]) * rates[`${endCurrency}`];
   };
 
   const onFormSubmit = (event) => {
@@ -38,64 +53,67 @@ const Form = ({ getResult}) => {
   };
 
   return (
-    <Calculator onSubmit={onFormSubmit}>
-      <Fieldset>
-        <Legend>Kalkulator Walut</Legend>
-        <p>
-          <label>
-            <Span>Kwota:</Span>
-            <Input
-              ref={ref}
-              value={amount}
-              onChange={({ target }) => setAmount(target.value)}
-              type="number"
-              name="amount"
-              min="0"
-              step="any"
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            <Span>Waluta początkowa:</Span>
-            <Input
-              as="select"
-              value={startCurrency}
-              onChange={({ target }) => setStartCurrency(target.value)}
-              name="startCurrency"
-            >
-              {Object.keys(rates).map((symbol => (
-                <option key={symbol} value={symbol}>
-                  {symbol}
-                </option>
-              )))}
+    <>
+      <Calculator onSubmit={onFormSubmit}>
+        <Fieldset>
+          <Legend>Kalkulator Walut</Legend>
+          <p>
+            <label>
+              <Span>Kwota:</Span>
+              <Input
+                ref={ref}
+                value={amount}
+                onChange={({ target }) => setAmount(target.value)}
+                type="number"
+                name="amount"
+                min="0"
+                step="any"
+              />
+            </label>
+          </p>
+          <p>
+            <label>
+              <Span>Waluta początkowa:</Span>
+              <Input
+                as="select"
+                value={startCurrency}
+                onChange={({ target }) => setStartCurrency(target.value)}
+                name="startCurrency"
+              >
+                {Object.keys(rates).map((symbol) => (
+                  <option key={symbol} value={symbol}>
+                    {symbol}
+                  </option>
+                ))}
+              </Input>
+            </label>
+          </p>
+          <p>
+            <label>
+              <Span>Waluta docelowa:</Span>
+              <Input
+                as="select"
+                value={endCurrency}
+                onChange={({ target }) => setEndCurrency(target.value)}
+                name="endCurrency"
+              >
+                {Object.keys(rates).map((symbol) => (
+                  <option key={symbol} value={symbol}>
+                    {symbol}
+                  </option>
+                ))}
+              </Input>
+            </label>
+          </p>
+          <p>
+            <Input as="button" special>
+              Przelicz
             </Input>
-          </label>
-        </p>
-        <p>
-          <label>
-            <Span>Waluta docelowa:</Span>
-            <Input
-              as="select"
-              value={endCurrency}
-              onChange={({ target }) => setEndCurrency(target.value)}
-              name="endCurrency"
-            >
-              {Object.keys(rates).map((symbol => (
-                <option key={symbol} value={symbol}>
-                  {symbol}
-                </option>
-              )))}
-            </Input>
-          </label>
-        </p>
-        <p>
-          <Input as="button" special>
-            Przelicz
-          </Input>
-        </p>
-      </Fieldset>
-    </Calculator>
+          </p>
+        </Fieldset>
+      </Calculator>
+      <Result result={result} />
+    </>
   );
 };
 
